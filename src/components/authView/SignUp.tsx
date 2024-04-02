0f744e932a7a4a105243771a766698d09fb03898
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-02 20:40:01 +0200

    textInput component added

--- src/components/authView/SignUp.tsx
+++ src/components/authView/SignUp.tsx
@@ -1,44 +1,40 @@
-import { useFormik } from "formik";
+import { Form, Formik, useFormik } from "formik";
+import { TextInput } from "../common/formikFields/TextInput";
 export const SignUp = () => {
-  const formik = useFormik({
-    initialValues: {
-      username: "",
-      email_address: "",
-      password: "",
-    },
-    //@TODO: validate
-    onSubmit(values) {
-      //@TODO: submit
-      // use FormikHelpers
-      console.log(values);
-    },
-  });
-
   return (
     <div id="signup">
-      <form onSubmit={formik.handleSubmit}>
-        <label>
-          <p>SignUp</p>
-
+      <Formik
+        initialValues={{
+          username: "",
+          email_address: "",
+          password: "",
+        }}
+        //@TODO: validate
+        onSubmit={(values) => {
+          //@TODO: submit
+          // use FormikHelpers
+          console.log(values);
+        }}
+      >
+        <Form>
           <label>
-            <p>E-mail address</p>
-            <input type="email" name="email_address" id="signup_email" />
+            <p>SignUp</p>
+
+            <TextInput
+              label="E-mail address"
+              type="email"
+              name="email_address"
+            />
+            {/* @TODO: show error if invalid and on blur */}
+
+            <TextInput label="Username" type="text" name="username" />
+            {/* @TODO: show error if invalid and on blur */}
+
+            <TextInput label="Password" type="password" name="password" />
             {/* @TODO: show error if invalid and on blur */}
           </label>
-
-          <label>
-            <p>Username</p>
-            <input type="text" name="username" id="signup_username" />
-            {/* @TODO: show error if invalid and on blur */}
-          </label>
-
-          <label>
-            <p>Password</p>
-            <input type="password" name="password" id="signup_password" />
-            {/* @TODO: show error if invalid and on blur */}
-          </label>
-        </label>
-      </form>
+        </Form>
+      </Formik>
     </div>
   );
 };
