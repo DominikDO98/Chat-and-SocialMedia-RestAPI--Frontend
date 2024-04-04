0a77ae68ebc34bbba688279c6e9cf551f96cd16d
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-04 21:50:27 +0200

    validation functions created and moved to AuthView, AuthFormik used instead of LogIn ang SignUp components

--- src/components/authView/AuthView.tsx
+++ src/components/authView/AuthView.tsx
@@ -1,11 +1,39 @@
-import { LogIn } from "./Login";
-import { SignUp } from "./SignUp";
+import { FormikValues } from "formik";
+import { ZodError } from "zod";
+import {
+  UserCreationSchema,
+  UserLoginByEmailSchema,
+  UserLoginByNameSchema,
+} from "../../utils/schemas/user.schemas";
+import { AuthFormik } from "./AuthFormik";
 
 export const AuthView = () => {
+  const loginValidatation = (values: FormikValues) => {
+    try {
+      if (!values.usename && !values.email_address)
+        UserCreationSchema.parse(values);
+      if (!values.username || (values.username && values.email_address))
+        UserLoginByEmailSchema.parse(values);
+      if (!values.email_address) UserLoginByNameSchema.parse(values);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return error.formErrors.fieldErrors;
+      }
+    }
+  };
+  const signupValidatation = (values: FormikValues) => {
+    try {
+      UserCreationSchema.parse(values);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return error.formErrors.fieldErrors;
+      }
+    }
+  };
   return (
     <div>
-      <LogIn />
-      <SignUp />
+      <AuthFormik label="Log In" validatation={loginValidatation} />
+      <AuthFormik label="Sign Up" validatation={signupValidatation} />
     </div>
   );
 };
