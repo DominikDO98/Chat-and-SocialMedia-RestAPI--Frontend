6a978ade5071dd1640372986cc26d0a887122ccb
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-04 21:12:02 +0200

    TextInput moved to common dir, <p> elements turn to <div>

---
+++ src/components/common/TextInput.tsx
@@ -0,0 +1,24 @@
+import { useField } from "formik";
+
+interface Props {
+  type: "email" | "text" | "password";
+  name: "email_address" | "username" | "password";
+  label: string;
+}
+
+export const TextInput = (props: Props) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label>
+        {props.label}
+        <div>
+          <input {...field} {...props} className="input" />
+          {meta.touched && meta.error ? (
+            <div className="error">{meta.error}</div>
+          ) : null}
+        </div>
+      </label>
+    </>
+  );
+};
