da36f98fa70969aacdeacc3a6122216923a2c84f
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-08 11:45:32 +0200

    jest config created

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "jest";
+const config: Config = {
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.(ts)?$": "ts-jest",
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+};
+
+export default config;
