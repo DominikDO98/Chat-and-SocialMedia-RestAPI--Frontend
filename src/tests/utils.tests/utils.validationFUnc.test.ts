f62dd30d2141720a55058d1e62f1c2fb62d51c22
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-10 11:43:36 +0200

    loginValidation func tests

---
+++ src/tests/utils.tests/utils.validationFUnc.test.ts
@@ -0,0 +1,26 @@
+import { loginValidation } from "../../utils/formik/validationFunc";
+import { UserCreationEnitity } from "../../utils/types/user.types";
+
+describe("formik validation functions tests", () => {
+  describe("loginValidation tests", () => {
+    test("loginValidation passes when correct data is provided", () => {
+      const goodValues: Omit<UserCreationEnitity, "id"> = {
+        username: "name5",
+        password: "password8",
+        email_address: "emailemail@wp.pl",
+      };
+      expect(loginValidation(goodValues)).toStrictEqual(undefined);
+    });
+    test("loginValidation throws error when incorrect data is provided", () => {
+      const data1: Omit<UserCreationEnitity, "id"> = {
+        username: "name",
+        password: "",
+        email_address: "emailemail",
+      };
+      expect(loginValidation(data1)).toStrictEqual({
+        email_address: ["Invalid email"],
+        password: ["String must contain at least 8 character(s)"],
+      });
+    });
+  });
+});
