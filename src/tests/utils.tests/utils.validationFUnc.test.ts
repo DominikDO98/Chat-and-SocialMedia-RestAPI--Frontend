b1e7f3df2fcd3c023ac503c6682a75cd0d36f874
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-11 21:12:18 +0200

    signupValidation func tests all configurations finished

--- src/tests/utils.tests/utils.validationFUnc.test.ts
+++ src/tests/utils.tests/utils.validationFUnc.test.ts
@@ -1,4 +1,7 @@
-import { loginValidation } from "../../utils/formik/validationFunc";
+import {
+  loginValidation,
+  signupValidation,
+} from "../../utils/formik/validationFunc";
 import { UserCreationEnitity } from "../../utils/types/user.types";
 
 describe("formik validation functions tests", () => {
@@ -36,6 +39,48 @@ describe("formik validation functions tests", () => {
         password: ["String must contain at least 8 character(s)"],
       });
       expect(loginValidation(data3)).toStrictEqual({
+        email_address: ["Invalid email"],
+        password: ["String must contain at least 8 character(s)"],
+        username: ["String must contain at least 5 character(s)"],
+      });
+    });
+  });
+  describe("signupValidation tests", () => {
+    test("signupValidation passes when correct data is provided", () => {
+      const goodValues: Omit<UserCreationEnitity, "id"> = {
+        username: "name5",
+        password: "password8",
+        email_address: "emailemail@wp.pl",
+      };
+      expect(signupValidation(goodValues)).toStrictEqual(undefined);
+    });
+    test("signupValidation throws error when incorrect data is provided", () => {
+      const data1: Omit<UserCreationEnitity, "id"> = {
+        username: "name",
+        password: "",
+        email_address: "emailemail",
+      };
+      const data2: Omit<UserCreationEnitity, "id"> = {
+        username: "name",
+        password: "",
+        email_address: "",
+      };
+      const data3: Omit<UserCreationEnitity, "id"> = {
+        username: "",
+        password: "",
+        email_address: "",
+      };
+      expect(signupValidation(data1)).toStrictEqual({
+        email_address: ["Invalid email"],
+        password: ["String must contain at least 8 character(s)"],
+        username: ["String must contain at least 5 character(s)"],
+      });
+      expect(signupValidation(data2)).toStrictEqual({
+        email_address: ["Invalid email"],
+        password: ["String must contain at least 8 character(s)"],
+        username: ["String must contain at least 5 character(s)"],
+      });
+      expect(signupValidation(data3)).toStrictEqual({
         email_address: ["Invalid email"],
         password: ["String must contain at least 8 character(s)"],
         username: ["String must contain at least 5 character(s)"],
