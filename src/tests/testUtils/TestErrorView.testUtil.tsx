8bd3477735692915b6bbf95126147c08f55e260c
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-12 16:19:19 +0200

    testUtils moved form errorView folder to tests folder

---
+++ src/tests/testUtils/TestErrorView.testUtil.tsx
@@ -0,0 +1,22 @@
+import { useHandleError } from "../../components/utils/hooks/useHandleError";
+interface Props {
+  message: string;
+}
+export const TestView = (props: Props) => {
+  const handleError = useHandleError();
+  const register = () => {
+    try {
+      throw new Error(props.message);
+    } catch (error) {
+      handleError(error as Error);
+    }
+  };
+
+  return (
+    <>
+      <button data-testId="fireError" onClick={register}>
+        ErrorButton
+      </button>
+    </>
+  );
+};
