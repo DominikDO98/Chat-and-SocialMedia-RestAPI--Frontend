3fc185bebf854aab580ad6d47bcbc4823ff87e9d
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-04-12 16:38:25 +0200

    AuthView.tests ext changed into tsx, added import for REact

--- src/tests/auth.tests/AuthView.test.tsx
+++ src/tests/auth.tests/AuthView.test.tsx
@@ -1,8 +1,9 @@
+import React from "react";
 import renderer from "react-test-renderer";
 import { AuthView } from "../../components/authView/AuthView";
 describe("AuthView tests", () => {
   test("AuthView matches a snapshot", () => {
-    const authView = renderer.create(AuthView()).toJSON();
+    const authView = renderer.create(<AuthView />).toJSON();
     expect(authView).toMatchSnapshot();
   });
 });
